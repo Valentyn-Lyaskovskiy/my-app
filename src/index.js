9c223868df4925e05b00327a9e822de783925cc5
Valentyn <valentyn.lyaskovskiy@gmail.com>
2022-11-07 16:33:56 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// const elem = <h2>REACT is a...</h2>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(elem, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
