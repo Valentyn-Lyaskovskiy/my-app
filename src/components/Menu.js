8300681883b5d2e26b717ee66bacf539869e78f4
Valentyn <riodemouro2017@gmail.com>
2023-03-18 20:45:44 +0200

    modules.css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,23 +1,33 @@
 import React from "react";
-
+import s from "./Menu.module.css";
 const Menu = () => {
   return (
     <nav className="menu">
       <ul>
         <li>
-          <a href="#">Profile</a>
+          <a className={s.link} href="#">
+            Profile
+          </a>
         </li>
         <li>
-          <a href="#">Messages</a>
+          <a className={s.link} href="#">
+            Messages
+          </a>
         </li>
         <li>
-          <a href="#">News</a>
+          <a className={s.link} href="#">
+            News
+          </a>
         </li>
         <li>
-          <a href="#">Music</a>
+          <a className={`${s.link} ${s.blue}`} href="#">
+            Music
+          </a>
         </li>
         <li>
-          <a href="#">Settings</a>
+          <a className={s.link} href="#">
+            Settings
+          </a>
         </li>
       </ul>
     </nav>
