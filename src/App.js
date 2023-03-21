87b9ec50e27e20e62f68fe2481454e90c086ef67
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-03-21 15:21:47 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Header from "./components/Header/Header";
@@ -7,17 +7,19 @@ import Menu from "./components/Menu/Menu";
 import Post from "./components/Profile/Post";
 import logo from "./logo.svg";
 
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
       <div className="wrapper">
         <Header />
         <Menu />
-        <Route Component={Dialogs} />
-        <Route Component={Profile} />
+        <Routes>
+          <Route path="/profile" element={<Post />} />
+          <Route path="/dialogs" element={<Dialogs />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
