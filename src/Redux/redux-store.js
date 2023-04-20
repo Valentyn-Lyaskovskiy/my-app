3902d08c8673760e2ed8d93a3f9dc6c511aafbe1
Valentyn <riodemouro2017@gmail.com>
2023-04-20 21:44:33 +0300

    Redux

---
+++ src/Redux/redux-store.js
@@ -0,0 +1,3 @@
+import { legacy_createStore } from "redux";
+
+let store = legacy_createStore();
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from './Redux/state';
+import store from "./Redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 let rerenderEntireTree = (state) => {
-  root.render(<App
-    state={state}
-    dispatch={store.dispatch.bind(store)}
-  />);
+  root.render(<App state={state} dispatch={store.dispatch.bind(store)} />);
 };
 
 rerenderEntireTree(store.getState());
